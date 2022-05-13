22e65f990b58a57aebfdc838de77b7bb872b11a0
RossT <rthomson412@gmail.com>
2022-05-13 09:36:13 -0400

    initial directory structure

---
+++ server.js
@@ -0,0 +1,24 @@
+const db = require("./db/connection");
+const express = require("express");
+const startInquirer = require("./lib/departments");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware express
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// (Not Found) Default error response
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Start server after connection established with database
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}. GROOVY!`);
+    startInquirer();
+  });
+});
